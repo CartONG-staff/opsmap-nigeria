906d43f7bdbdf71f6650e8144a64607ac64c5db1
Etienne Delclaux <e_delclaux@cartong.org>
2021-02-26 16:29:43 -0400

    vueconfigjs: export all the lib via webpack

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-// Ensure default export behavior
+// Ensure "" export behavior : "" === everything.
 // It is needed because the project is a lib.
 // cf: https://cli.vuejs.org/guide/build-targets.html#vue-vs-js-ts-entry-files
 
 module.exports = {
   configureWebpack: {
     output: {
-      libraryExport: "default"
+      libraryExport: ""
     }
   }
 };
