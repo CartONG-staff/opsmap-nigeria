fa3663512414f72145c7e028f9f17c850b5f5099
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-01 16:16:48 -0400

    add componenttest.vue

--- src/entry-point-app.vue
+++ src/entry-point-app.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <TKHeader source="generic lib - HEADER" />
+    <TKComponentTest />
     <TKFooter source="generic lib - FOOTER" />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script lang="ts">
 import Vue from "vue";
 
+import TKComponentTest from "./components/TKComponentTest.vue";
 import TKFooter from "./components/TKFooter.vue";
 import TKHeader from "./components/TKHeader.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
+    TKComponentTest,
     TKFooter,
     TKHeader
   },
--- src/entry-point-lib.ts
+++ src/entry-point-lib.ts
@@ -1,6 +1,8 @@
+import TKComponentTest from "./components/TKComponentTest.vue";
 import TKFooter from "./components/TKFooter.vue";
 import TKHeader from "./components/TKHeader.vue";
 
+export { TKComponentTest };
 export { TKFooter };
 export { TKHeader };
-export default { TKFooter, TKHeader };
+export default { TKComponentTest, TKFooter, TKHeader };
