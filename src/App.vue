8df2481a373183863bfd9e27cc5c81b7b73fb59a
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-01 11:49:33 -0400

    Add 'build + serve' target

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <TKHeader source="generic lib - HEADER" />
+    <TKFooter source="generic lib - FOOTER" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import TKFooter from "./components/TKFooter.vue";
+import TKHeader from "./components/TKHeader.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    TKFooter,
+    TKHeader
+  },
+  methods: {}
+});
+</script>
+
+<style></style>
