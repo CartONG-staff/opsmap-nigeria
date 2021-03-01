22af45ace46c7339eddd14a994c55688b9d4fd0a
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-01 16:40:09 -0400

    dummy combobox added

---
+++ src/components/TKComponentTest.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="foo">
+    <v-combobox
+      clearable
+      hide-selected
+      persistent-hint
+      placeholder="Select a state"
+      prefix="State:"
+    >
+    </v-combobox>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TKComponentTest",
+  props: ["source"]
+});
+</script>
+
+<style scoped></style>
