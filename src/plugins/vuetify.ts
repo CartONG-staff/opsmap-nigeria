533f4ef9359978c61977b584d463048b49751166
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-10 12:47:33 -0400

    add theming in comments : Nigeria style

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,3 +9,18 @@ import Vuetify from "vuetify/lib/framework";
 Vue.use(Vuetify);
 
 export default new Vuetify({});
+
+
+// NIGERIA CONFIG
+// export default new Vuetify({
+//     theme: {
+//       themes: {
+//         light: {
+//           primary: '#286090',
+//           secondary: '#337FDD',
+//           accent: '#8c9eff',
+//           error: '#b71c1c',
+//         },
+//       },
+//     },
+//   });
