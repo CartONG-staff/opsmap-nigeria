1d85aef6167b728bd2dde862f091c00d44286506
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-22 10:06:53 -0400

    add Map empty component

--- src/components/TKMainComponent/TKHomePage/TKHomePage.vue
+++ src/components/TKMainComponent/TKHomePage/TKHomePage.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="tk-homepage"></div>
+  <div class="tk-homepage"><TKMap /></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
+import TKMap from "@/components/TKMainComponent/TKMap";
 @Component({
-  components: {},
+  components: { TKMap },
 })
 export default class TKHomePage extends Vue {}
 </script>
---
+++ src/components/TKMainComponent/TKMap/TKMap.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="tk-map"></div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TKMap extends Vue {}
+</script>
+
+<style lang="css" scoped>
+.tk-map {
+  background-color: purple;
+  width: 200px;
+  height: 200px;
+  border-radius: 15px;
+}
+</style>
