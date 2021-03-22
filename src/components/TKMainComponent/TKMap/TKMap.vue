98dfe6221d198b871a17fb58bef0a8a04c6646f3
Etienne Delclaux <e_delclaux@cartong.org>
2021-03-22 10:21:35 -0400

    add mapbox + types packages - working example

--- src/components/TKMainComponent/TKMap/TKMap.vue
+++ src/components/TKMainComponent/TKMap/TKMap.vue
@@ -1,16 +1,30 @@
 <template lang="html">
-  <div class="tk-map"></div>
+  <div id="tk-map"></div>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2FydG9uZyIsImEiOiJjazJldzVobGkwOWRxM2hzNTB1M3o2cG94In0.w7FyG31FWqXm3vXSh6WtxQ";
 
 @Component
-export default class TKMap extends Vue {}
+export default class TKMap extends Vue {
+  map!: mapboxgl.Map;
+
+  mounted() {
+    this.map = new mapboxgl.Map({
+      container: "tk-map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-68.48130213973545, -54.8458772648677],
+      zoom: 12,
+    });
+  }
+}
 </script>
 
 <style lang="css" scoped>
-.tk-map {
+#tk-map {
   background-color: purple;
   width: 200px;
   height: 200px;
