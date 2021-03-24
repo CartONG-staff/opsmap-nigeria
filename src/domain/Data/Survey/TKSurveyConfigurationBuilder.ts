c5a5dc5b14ae8673f880768c543348fbc9987ee7
Olivier <o_ribiere@cartong.org>
2021-03-24 20:05:06 +0100

    Add Labels and Categories Collection builders

--- src/domain/Data/Survey/TKSurveyConfigurationBuilder.ts
+++ src/domain/Data/Survey/TKSurveyConfigurationBuilder.ts
@@ -2,24 +2,44 @@ import { TKCSVReader } from "../CSV/TKCSVReader";
 import { TKCSVSurveyInfo } from "../CSV/TKCSVTypes";
 import { TKKoboSurveyInfo } from "../Kobo/TKKoboSurveyInfo";
 import {
-  TKTrafficLightsCollectionBuilder,
+  TKCategory,
+  TKCategoriesCollection,
+  TKCategoriesCollectionBuilder,
+} from "./TKCategoriesCollectionBuilder";
+import {
   TKTrafficLightsRaw,
   TKTrafficLightsCollection,
+  TKTrafficLightsCollectionBuilder,
 } from "./TKTrafficLightsCollectionBuilder";
+import {
+  TKLabel,
+  TKLabelsCollection,
+  TKLabelsCollectionBuilder,
+} from "./TKLabelsCollectionBuilder";
 
 interface SurveyConfiguration {
+  categories: TKCategoriesCollection;
   trafficLights: TKTrafficLightsCollection;
+  labels: TKLabelsCollection;
 }
 
 export async function TKSurveyConfigurationBuilder(
   survey: TKKoboSurveyInfo | TKCSVSurveyInfo
 ): SurveyConfiguration {
+  const rawCategories: TKCategory[] = await TKCSVReader(
+    "categories",
+    survey.folder,
+    true
+  );
   const rawTrafficLights: TKTrafficLightsRaw[] = await TKCSVReader(
     "traffic_lights",
     survey.folder,
     true
   );
+  const rawLabels: TKLabel[] = await TKCSVReader("labels", survey.folder, true);
   return {
+    categories: TKCategoriesCollectionBuilder(rawCategories),
     trafficLights: TKTrafficLightsCollectionBuilder(rawTrafficLights),
+    labels: TKLabelsCollectionBuilder(rawLabels),
   };
 }
