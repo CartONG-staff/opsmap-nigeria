7c9ef09ebf57a16928f255bf9ec4b48b654c5006
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-02 15:49:21 -0400

    update surveyitems indentation

--- src/components/TKMainComponent/TKCampComponents/TKSurveyVisualizer/TKSurveyItemI.ts
+++ src/components/TKMainComponent/TKCampComponents/TKSurveyVisualizer/TKSurveyItemI.ts
@@ -1,11 +1,11 @@
 export enum TrafficLight {
-    OK = 1,
-    WARNING = 2,
-    CRITICAL = 3
+  OK = 1,
+  WARNING = 2,
+  CRITICAL = 3,
 }
 
 export interface TKSurveyItemI {
-    name: string;
-    value: string;
-    trafficLight?: TrafficLight;
-  }
+  name: string;
+  value: string;
+  trafficLight?: TrafficLight;
+}
