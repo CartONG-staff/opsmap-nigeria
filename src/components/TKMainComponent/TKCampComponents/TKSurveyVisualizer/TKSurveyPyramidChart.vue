5e8f3061c5d602ee47da399ee3bbbe2164caaf9d
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-06 12:56:59 -0400

    add callback to update legend value (no neg)

--- src/components/TKMainComponent/TKCampComponents/TKSurveyVisualizer/TKSurveyPyramidChart.vue
+++ src/components/TKMainComponent/TKCampComponents/TKSurveyVisualizer/TKSurveyPyramidChart.vue
@@ -69,36 +69,23 @@ export default class TKSurveyPyramidChart extends Vue {
   config: ChartConfiguration = {
     type: "bar",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: [
+        "60 or older",
+        "36-59 years",
+        "18-35 years",
+        "6-17 years",
+        "0-5 years"
+      ],
       datasets: [
         {
-          label: "# of Votes",
-          data: [12, 19, -3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132)",
-            "rgba(54, 162, 235)",
-            "rgba(255, 206, 86)",
-            "rgba(75, 192, 192)",
-            "rgba(153, 102, 255)",
-            "rgba(255, 159, 64)"
-          ],
-          // backgroundColor: [
-          //   "rgba(255, 99, 132, 0.2)",
-          //   "rgba(54, 162, 235, 0.2)",
-          //   "rgba(255, 206, 86, 0.2)",
-          //   "rgba(75, 192, 192, 0.2)",
-          //   "rgba(153, 102, 255, 0.2)",
-          //   "rgba(255, 159, 64, 0.2)"
-          // ],
-          // borderColor: [
-          //   "rgba(255, 99, 132, 1)",
-          //   "rgba(54, 162, 235, 1)",
-          //   "rgba(255, 206, 86, 1)",
-          //   "rgba(75, 192, 192, 1)",
-          //   "rgba(153, 102, 255, 1)",
-          //   "rgba(255, 159, 64, 1)"
-          // ],
-          borderWidth: 1
+          label: "Female",
+          data: [12, 19, 3, 5, 0],
+          backgroundColor: "#f37788"
+        },
+        {
+          label: "Male",
+          data: [-4, -8, -1, -14, 0],
+          backgroundColor: "#4095cd"
         }
       ]
     },
@@ -109,6 +96,20 @@ export default class TKSurveyPyramidChart extends Vue {
         padding: 20
       },
       scales: {
+        x: {
+          ticks: {
+            callback: (value, _index, _values): string => {
+              let mynumber = 0;
+              if (typeof value === "string") {
+                mynumber = parseInt(value);
+              } else if (typeof value === "number") {
+                mynumber = value;
+              }
+              mynumber = mynumber < 0 ? 0 - mynumber : mynumber;
+              return mynumber.toString();
+            }
+          }
+        },
         y: {
           beginAtZero: true,
           stacked: true
@@ -126,6 +127,13 @@ export default class TKSurveyPyramidChart extends Vue {
         legend: {
           position: "top",
           reverse: true
+        },
+        tooltip: {
+          callbacks: {
+            label: function(context) {
+              return "";
+            }
+          }
         }
       }
       // scales: {
