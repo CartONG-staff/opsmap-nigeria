32539deec8fa94cfdfda394cd68cf805fae0b933
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-12 10:53:45 -0400

    update dataaaaa name to data

--- src/domain/dataaaaa/logos/index.ts
+++ src/domain/dataaaaa/logos/index.ts
@@ -1,6 +1,6 @@
-import { TKLOGO_CARTONG } from "@/domain/dataaaaa/logos/TKLogoCartONG";
-import { TKLOGO_UNHCR } from "@/domain/dataaaaa/logos/TKLogoUNHCR"
-import { TKLOGO_REACH } from "@/domain/dataaaaa/logos/TKLogoReach";
-import { TKLOGO_CCCM } from "@/domain/dataaaaa/logos/TKLogoCCCM";
+import { TKLOGO_CARTONG } from "@/domain/data/logos/TKLogoCartONG";
+import { TKLOGO_UNHCR } from "@/domain/data/logos/TKLogoUNHCR"
+import { TKLOGO_REACH } from "@/domain/data/logos/TKLogoReach";
+import { TKLOGO_CCCM } from "@/domain/data/logos/TKLogoCCCM";
 
 export { TKLOGO_CARTONG, TKLOGO_CCCM, TKLOGO_UNHCR, TKLOGO_REACH }
