696f21dd3dd891ead512935f5427ef97540ff2e5
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-12 11:01:10 -0400

    force update of logo case

--- src/domain/data/Logoss/TKLogoCartONG.ts
+++
@@ -1,9 +0,0 @@
-import { TKLogo } from "@/domain/ui/TKLogo";
-
-export const TKLOGO_CARTONG: TKLogo = {
-  name: "CartONG",
-  urlLogo:
-    "https://www.cartong.org/sites/cartong/files/images/CartONG_logo_long_0_0_0.png",
-  urlRedirection: "https://www.cartong.org/",
-}
-
