8612ae1abf3a187c49cbf1387074d9150d108a31
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-14 15:23:07 -0400

    addtwo level in TKSurvey: camp + date

--- src/domain/core/TKSurvey.ts
+++ src/domain/core/TKSurvey.ts
@@ -2,7 +2,7 @@ import { TKCampDescription } from "./TKCampDescription"
 import { TKBoundariesCollection } from "./TKBoundariesCollection"
 import { TKSubmissions } from "./TKSubmissions";
 export interface TKSurvey {
-    submissionsByCamps: {[campId: string]: TKSubmissions};
+    submissionsByCamps: {[campId: string]: {[date: string]: TKSubmissions};};
     campsList: TKCampDescription[];
     boundariesList: TKBoundariesCollection;
 }
