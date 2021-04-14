81e707ba5041446900f7b08a6f3bd474bf337cfd
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-14 17:37:04 -0400

    remove an extra } in submission

--- src/domain/core/TKSubmission.ts
+++ src/domain/core/TKSubmission.ts
@@ -1,6 +1,3 @@
 import { TKSubmissionThematic } from "./TKSubmissionThematic";
 
 export interface TKSubmission extends Record<string, TKSubmissionThematic> {}
-}
-
-// TODO: a more flexible approach for language.
