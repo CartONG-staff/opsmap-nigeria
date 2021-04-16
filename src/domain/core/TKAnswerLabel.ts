0c3afcf2be8d47421034d417dfb56d91862cb3a1
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-16 16:29:52 -0400

    crate answer and field label in core

---
+++ src/domain/core/TKAnswerLabel.ts
@@ -0,0 +1,6 @@
+export interface TKAnswerLabel {
+    choice_name: string;
+    choice_name_en: string;
+    choice_name_fr?: string;
+    choice_name_pt?: string;
+}
---
+++ src/domain/core/TKFieldLabel.ts
@@ -0,0 +1,6 @@
+export interface TKFieldLabel {
+    field_name: string;
+    field_label_en: string;
+    field_label_fr?: string;
+    field_label_pt?: string;
+}
