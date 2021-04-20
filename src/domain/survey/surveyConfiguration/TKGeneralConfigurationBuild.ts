2f4f663d11b06e6cd09e397aa6240eec376dc818
Etienne Delclaux <e_delclaux@cartong.org>
2021-04-20 17:38:50 -0400

    reactivate default uboundaries mask

--- src/domain/survey/surveyConfiguration/TKGeneralConfigurationBuild.ts
+++ src/domain/survey/surveyConfiguration/TKGeneralConfigurationBuild.ts
@@ -60,7 +60,7 @@ export async function  TKGeneralConfigurationBuild(): Promise<TKGeneralConfigura
             adm2Name: dict["adm2Name"] ?? "ggi_city",
             adm3Pcode: dict["adm3Pcode"] ?? "",
             adm3Name: dict["adm3Name"] ?? "ggi_address",
-            useBoundariesMasks: false
+            useBoundariesMasks: true
         },
         indicatorsDescription:{
             home:[{
