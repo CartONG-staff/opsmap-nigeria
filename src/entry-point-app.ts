968519dc3562519bdd62a084fbbb9e5f962e57c8
Etienne Delclaux <e_delclaux@cartong.org>
2021-05-05 14:55:12 -0400

    remove vue-analytics

--- src/entry-point-app.ts
+++ src/entry-point-app.ts
@@ -2,13 +2,8 @@ import Vue from "vue";
 import TKApp from "@/app/TKApp.vue";
 import vuetify from "@/plugins/vuetify";
 import i18n from "@/i18n";
-import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueAnalytics, {
-  id: 'G-9VDDG9K108'
-});
 
 new Vue({
   vuetify,
