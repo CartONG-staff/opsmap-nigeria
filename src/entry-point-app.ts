6f90db0fc4c5a739d810e3d46d87ec795e506b60
Etienne Delclaux <e_delclaux@cartong.org>
2021-05-05 09:27:16 -0400

    test of adding google analytics

--- src/entry-point-app.ts
+++ src/entry-point-app.ts
@@ -2,8 +2,14 @@ import Vue from "vue";
 import TKApp from "@/app/TKApp.vue";
 import vuetify from "@/plugins/vuetify";
 import i18n from "@/i18n";
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'G-9VDDG9K108'
+});
+
 new Vue({
   vuetify,
   i18n,
