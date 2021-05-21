39f81d21ca824eed3cdfe72963aecca05409f36c
Etienne Delclaux <e_delclaux@cartong.org>
2021-05-21 08:59:22 -0400

    update mapbox token

--- src/domain/opsmapConfig/TKOpsmapConfiguration.ts
+++ src/domain/opsmapConfig/TKOpsmapConfiguration.ts
@@ -199,7 +199,7 @@ export async function TKReadGeneralConfiguration(
     // token: "pk.eyJ1Ijoib3BzbWFwcGVyIiwiYSI6ImNrbW5xMWFuYzBqejMydnBnN2VjMTBj;cG8ifQ.OtWWd9kzJdJjogrY7gb-sw",
     // style: "mapbox://styles/opsmapper/ckmnq4jfb12r217o7yon9r383",
     mapConfig: {
-      token: "pk.eyJ1IjoidW5oY3IiLCJhIjoiOUQzQ2dnbyJ9.6ghfFmvxpu7HvHzXci_ogw",
+      token: "pk.eyJ1IjoidW5oY3IiLCJhIjoiY2tveWJlcDV5MDVycTJ2and3ZXllcW1leCJ9.Vp5XDh5OhDXxZCZUvgEuDg",
       style: "mapbox://styles/unhcr/ckok20x8h03ma18qp76mxi3u4",
       padding: 100,
       zoomspeed: 2
