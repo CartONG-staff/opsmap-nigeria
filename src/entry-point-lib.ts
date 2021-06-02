f53f356c61c779bd53321ad8b527a6a3ee8d4907
Etienne Delclaux <e_delclaux@cartong.org>
2021-06-02 16:30:45 -0400

    remove lib dimension

--- src/entry-point-lib.ts
+++
@@ -1,8 +0,0 @@
-// /**
-//  * This the entry point of the project. Only what's exported here is acccessible though the lib
-//  * @module
-//  * @internal
-//  */
-
-import TKApp from "@/app/TKApp.vue";
-export { TKApp };
