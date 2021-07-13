8bd8ef9d7ba89dea1b07cef5ac73d737c6264372
Etienne Delclaux <e_delclaux@cartong.org>
2021-07-13 12:07:26 -0400

    use BASE_URL in csv read

--- src/domain/csv/TKCSVReader.ts
+++ src/domain/csv/TKCSVReader.ts
@@ -12,7 +12,7 @@ export async function TKCSVRead<T>(
   header: boolean
 ): Promise<T> {
   return new Promise((resolve, reject) => {
-    parse(`/data/${folder}/${name}.csv`, {
+    parse(`${process.env.BASE_URL}/data/${folder}/${name}.csv`, {
       header: header,
       download: true,
       encoding: "utf-8",
