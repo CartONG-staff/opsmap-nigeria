5d77f1834aafe7f91019a043fe7d11dbeeaff86f
Etienne Delclaux <e_delclaux@cartong.org>
2021-07-26 11:47:24 -0400

    add empty PDF document

--- src/domain/export/TKPDFWriter.ts
+++ src/domain/export/TKPDFWriter.ts
@@ -1,19 +1,26 @@
 import { TKDatasetFilterer } from "@/domain/survey/TKDatasetFilterer";
 import { TKComputeExportFilename } from "./TKExportCommon";
 
+import { jsPDF, jsPDFOptions } from "jspdf";
+
 // ////////////////////////////////////////////////////////////////////////////
 // Temaplted Read method for csv inputs
 // ////////////////////////////////////////////////////////////////////////////
 
 export function TKPDFWrite(dataset: TKDatasetFilterer, locale: string) {
   if (dataset.currentSubmission) {
+    const documentTitle = TKComputeExportFilename(dataset, "pdf");
     const pdfContent = "";
+
+    const doc = new jsPDF();
+    doc.save(documentTitle);
+
     //computeCSVContent(dataset.currentSubmission, locale);
-    const encodedUri = encodeURI(pdfContent);
-    const link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", TKComputeExportFilename(dataset, "pdf")); // filename
-    document.body.appendChild(link); // Required for FF ?
-    link.click();
+    // const encodedUri = encodeURI(pdfContent);
+    // const link = document.createElement("a");
+    // link.setAttribute("href", encodedUri);
+    // link.setAttribute("download", documentTitle); // filename
+    // document.body.appendChild(link); // Required for FF ?
+    // link.click();
   }
 }
