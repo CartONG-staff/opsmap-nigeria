52d929a4d7dda582d49fa189a167849ae5a80b2b
Olivier Ribiere <o_ribiere@cartong.org>
2021-07-31 12:44:02 +0200

    Add pcode fields namefor UNHCR DB in general config

--- src/domain/opsmapConfig/TKSpatialDescription.ts
+++ src/domain/opsmapConfig/TKSpatialDescription.ts
@@ -12,9 +12,11 @@ export interface TKSpatialDescription {
   siteIDField: string;
   siteLastUpdateField: string;
   siteTypeField: string;
-  adm1Pcode: string;
+  adm1Pcode: string;//pcode field in dataset
+  adm1DBPcode:string;//pcode field in UNHCR DB
   adm1Name: string;
-  adm2Pcode: string;
+  adm2Pcode: string;//pcode field in dataset
+  adm2DBPcode:string;//pcode field in UNHCR DB
   adm2Name: string;
   adm3Pcode: string;
   adm3Name: string;
