8d4159d5522f966a7fe5ff3a77137c92ff7807ac
Olivier Ribiere <o_ribiere@cartong.org>
2021-07-31 13:50:43 +0200

    Fix bug on maths traffic lights

--- src/domain/survey/TKSubmissionEntryText.ts
+++ src/domain/survey/TKSubmissionEntryText.ts
@@ -58,8 +58,8 @@ function getTrafficLightColor(
       if (!result.includes(false)) {
         match = item.color;
       }
-      return match === undefined ? TKTrafficLightValues.UNDEFINED : match;
     }
+    return match === undefined ? TKTrafficLightValues.UNDEFINED : match;
   }
   if (trafficLight.type === TKFDFTrafficLightTypes.LIST) {
     const match = trafficLight.values
