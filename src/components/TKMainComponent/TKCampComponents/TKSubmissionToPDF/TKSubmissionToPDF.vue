b4ad44150c4b5e110d268c9ec5a3be7ebc2a4978
Etienne Delclaux <e_delclaux@cartong.org>
2021-08-19 12:41:07 -0400

    add TrafficLightSupport

--- src/components/TKMainComponent/TKCampComponents/TKSubmissionToPDF/TKSubmissionToPDF.vue
+++ src/components/TKMainComponent/TKCampComponents/TKSubmissionToPDF/TKSubmissionToPDF.vue
@@ -20,7 +20,11 @@ import { TKDatasetFilterer } from "@/domain/survey/TKDatasetFilterer";
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { TKSubmissionVisualizerOptions } from "../TKSubmissionVisualizer";
 import jsPDF from "jspdf";
-import autoTable, { MarginPaddingInput, UserOptions } from "jspdf-autotable";
+import autoTable, {
+  MarginPaddingInput,
+  RowInput,
+  UserOptions
+} from "jspdf-autotable";
 
 import { TKComputeExportFilename } from "@/domain/export/TKExportCommon";
 import TKSubmissionToPDFHeader from "./TKSubmissionToPDFHeader.vue";
@@ -30,6 +34,7 @@ import TKSubmissionToPDFIndicators from "./TKSubmissionToPDFIndicators.vue";
 import { TKGetLocalValue } from "@/domain/ui/TKLabel";
 import { TKIconUrl } from "@/domain/ui/TKIcons";
 import { TKSubmissionThematic } from "@/domain/survey/TKSubmissionThematic";
+import { TKTrafficLightValues } from "@/domain/fdf/TKTrafficLightValues";
 
 @Component({
   components: {
@@ -123,18 +128,23 @@ export default class TKSubmissionToPDF extends Vue {
   ): UserOptions {
     const thematicHeaderImageURL = TKIconUrl(thematic.iconFileName);
     const thematicDataHeader = [
-      { key: TKGetLocalValue(thematic.nameLabel, this.$i18n.locale), value: "" }
+      {
+        key: TKGetLocalValue(thematic.nameLabel, this.$i18n.locale),
+        value: ""
+      }
     ];
     const thematicDataBody = thematic.data.map(item => {
       if (item.type === "text") {
         return {
           key: TKGetLocalValue(item.fieldLabel, this.$i18n.locale),
-          value: TKGetLocalValue(item.answerLabel, this.$i18n.locale)
+          value: TKGetLocalValue(item.answerLabel, this.$i18n.locale),
+          trafficLightColor: item.trafficLightColor
         };
       }
       return {
         key: "-",
-        value: "-"
+        value: "-",
+        trafficLightColor: false
       };
     });
     thematicDataBody.push(...thematicDataBody);
@@ -143,10 +153,9 @@ export default class TKSubmissionToPDF extends Vue {
     const cellFontSize = 9;
     return {
       head: thematicDataHeader,
-      body: thematicDataBody,
+      body: thematicDataBody as RowInput[], // Remove warning
       startY: startY,
       margin: margins,
-      styles: {},
       columnStyles: {
         key: { halign: "left", fontSize: cellFontSize },
         value: { halign: "right", fontSize: cellFontSize, fontStyle: "bold" }
@@ -172,9 +181,26 @@ export default class TKSubmissionToPDF extends Vue {
             data.cell.styles.cellPadding = {
               left: 5 * thematicHeadMargins
             };
-            data.cell.width = 178;
+            data.cell.width = 179;
           } else if (data.column.dataKey === "value") {
             data.cell.width = 0;
+          }
+        } else if (data.row.section === "body") {
+          if (data.column.dataKey === "value") {
+            switch ((data.row.raw as any).trafficLightColor) {
+              case TKTrafficLightValues.OK:
+                pdf.setTextColor(21, 120, 21);
+                break;
+              case TKTrafficLightValues.WARNING:
+                pdf.setTextColor(255, 204, 0);
+                break;
+              case TKTrafficLightValues.DANGER:
+                pdf.setTextColor(204, 112, 0);
+                break;
+              case TKTrafficLightValues.CRITICAL:
+                pdf.setTextColor(204, 10, 0);
+                break;
+            }
           }
         }
       },
