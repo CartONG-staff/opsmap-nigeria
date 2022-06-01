d42614e9fb4fb6e4da3e87bd1926b53792b30ee1
Etienne Delclaux <e_delclaux@cartong.org>
2022-06-01 08:55:10 +0200

    feat (chart): background set to white

--- src/primary/components/TKMainComponent/TKCampComponents/TKSubmissionVisualizer/TKSubmissionEntryView.vue
+++ src/primary/components/TKMainComponent/TKCampComponents/TKSubmissionVisualizer/TKSubmissionEntryView.vue
@@ -1,22 +1,24 @@
 <template lang="html">
   <div class="tk-submission-entry-container">
     <TKSubmissionEntryTextView v-if="entry.type === 'text'" :entry="entry" />
-    <TKSubmissionEntryAgePyramidChart
-      class="tk-chart"
-      v-else-if="entry.type === 'age_pyramid'"
-      :entry="entry"
-    />
-    <TKSubmissionEntryDoughnutChart
-      class="tk-chart"
-      v-else-if="entry.type === 'doughnut'"
-      :entry="entry"
-    />
-    <TKSubmissionEntryPolarChart
-      class="tk-chart"
-      v-else-if="entry.type === 'polar'"
-      :entry="entry"
-    />
-    <div class="tk-hseparator" />
+    <div class="tk-chart-container" v-else>
+      <TKSubmissionEntryAgePyramidChart
+        class="tk-chart"
+        v-if="entry.type === 'age_pyramid'"
+        :entry="entry"
+      />
+      <TKSubmissionEntryDoughnutChart
+        class="tk-chart"
+        v-else-if="entry.type === 'doughnut'"
+        :entry="entry"
+      />
+      <TKSubmissionEntryPolarChart
+        class="tk-chart"
+        v-else-if="entry.type === 'polar'"
+        :entry="entry"
+      />
+    </div>
+    <div v-if="entry.type === 'text'" class="tk-hseparator" />
   </div>
 </template>
 
@@ -49,12 +51,16 @@ export default class TKSubmissionentryView extends Vue {
   height: 1px;
   width: 100%;
 }
-
-.tk-chart {
+.tk-chart-container {
   margin-top: -1px;
-  background-color: transparent;
-  border-radius: 3px;
-  padding-top: 15px;
-  padding-bottom: 15px;
+  background-color: var(--v-thematicBackground-base);
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+.tk-chart {
+  background-color: white;
+  border-radius: 15px;
+  padding-top: 5px;
+  padding-bottom: 5px;
 }
 </style>
