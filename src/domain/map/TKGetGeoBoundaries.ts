93ecfb7c51771dbc34ea1c4550526e52d1408b94
Etienne Delclaux <e_delclaux@cartong.org>
2022-11-06 09:21:30 +0100

    feat(adm1): use unhcr db query result

--- src/domain/map/TKGetGeoBoundaries.ts
+++ src/domain/map/TKGetGeoBoundaries.ts
@@ -1,9 +1,24 @@
 import { ArcgisServerDataGetter } from "@/secondary/arcgis/TKArcgisServerDataGetter";
 import { TKGeoDataset } from "@/domain/map/TKGeoDataset";
-import { FeatureCollection } from "geojson";
 import { TKDataset } from "@/domain/survey/TKDataset";
 import { TKFDFSpatialDescription } from "@/domain/fdf/TKFDFSpatialDescription";
-import TKConfigurationModule from "@/store/modules/configuration/TKConfigurationModule";
+
+function computeAdmin1Query(admin1List: string[], adm1Ref: string): string {
+  return admin1List.map(adm1 => `${adm1Ref} = '${adm1}'`).join(" OR ");
+}
+
+function computeAdmin2Query(
+  admin1List: string[],
+  adm2RefInAdm1: string
+): string {
+  let admin2WhereClause = "";
+  admin1List.map(x => (admin2WhereClause += `${adm2RefInAdm1} = '${x}' OR `));
+  admin2WhereClause = admin2WhereClause.substring(
+    0,
+    admin2WhereClause.length - 3
+  );
+  return admin2WhereClause;
+}
 
 export async function TKGetGeoBoundaries(
   dataset: TKDataset,
@@ -11,40 +26,29 @@ export async function TKGetGeoBoundaries(
 ): Promise<TKGeoDataset> {
   const before = Date.now();
 
-  // Get Admin1 from file in public
-  let admin1GeoData: FeatureCollection = {
-    type: "FeatureCollection",
-    features: []
-  };
-  await fetch(
-    `${process.env.BASE_URL}/${TKConfigurationModule.configuration.spatialConfiguration.localFiles.admin1LocalURL}`
-  )
-    .then(response => response.json())
-    .then(json => {
-      admin1GeoData = json;
-    });
-
-  // Get Admin2 from unhcr server
+  // Get full admin1 List
   let admin1List: string[] = [];
   for (const survey of dataset.surveys) {
     survey.boundaries.admin1.map(x => admin1List.push(x.pcode));
   }
-
   admin1List = [...new Set(admin1List)];
-  let admin2WhereClause = "";
 
-  admin1List.map(
-    x =>
-      (admin2WhereClause += `${spatialDescription.adm2RefInAdm1} = '${x}' OR `)
-  );
-  admin2WhereClause = admin2WhereClause.substring(
-    0,
-    admin2WhereClause.length - 3
-  );
+  const admin1Query = computeAdmin1Query(admin1List, "pcode");
+  const admin1GeoData = await new ArcgisServerDataGetter(
+    encodeURI("core/wrl_adm1_polbnd_a_unhcr/FeatureServer/0"),
+    encodeURI(admin1Query),
+    true,
+    "geojson"
+  ).getData();
 
+  // Get Admin2 from unhcr server
+  const admin2Query = computeAdmin2Query(
+    [...new Set(admin1List)],
+    spatialDescription.adm2RefInAdm1
+  );
   const admin2GeoData = await new ArcgisServerDataGetter(
     encodeURI("core/wrl_adm2_polbnd_a_unhcr/FeatureServer/0"),
-    encodeURI(admin2WhereClause),
+    encodeURI(admin2Query),
     true,
     "geojson"
   ).getData();
