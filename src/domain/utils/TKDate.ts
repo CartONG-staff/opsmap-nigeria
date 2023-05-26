9a021df646530aa564ffc2cb106de1d586d4a644
Etienne Delclaux <e_delclaux@cartong.org>
2023-05-26 11:58:29 +0200

    feat(date): date comparison now based on momentjs

--- src/domain/utils/TKDate.ts
+++ src/domain/utils/TKDate.ts
@@ -1,24 +1,20 @@
 import moment from "moment";
 
-export function TKDateCompare(date1: string, date2: string): number {
-  const asplitted = date1.split("/");
-  const bsplitted = date2.split("/");
-  if (asplitted.length !== 3 || bsplitted.length !== 3) {
+export function TKDateCompare(
+  date1: string,
+  date2: string,
+  displayDateFormat: string
+): number {
+  const date1Moment = moment(date1, displayDateFormat);
+  const date2Moment = moment(date2, displayDateFormat);
+
+  if (date1Moment.isSame(date2Moment)) {
     return 0;
   }
-  const adated = new Date(
-    parseInt(asplitted[2]),
-    parseInt(asplitted[1]) - 1,
-    parseInt(asplitted[0])
-  );
-  const bdated = new Date(
-    parseInt(bsplitted[2]),
-    parseInt(bsplitted[1]) - 1,
-    parseInt(bsplitted[0])
-  );
-  if (adated < bdated) return 1;
-  else if (adated > bdated) return -1;
-  else return 0;
+  if (date1Moment.isBefore(date2Moment)) {
+    return -1;
+  }
+  return 1;
 }
 
 // ////////////////////////////////////////////////////////////////////////////
