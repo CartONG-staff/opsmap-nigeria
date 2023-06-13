407efaa860ac9b88201630ef4b99b86d79d631a6
Etienne Delclaux <e_delclaux@cartong.org>
2023-06-13 09:47:18 +0200

    feat(icons): move icons patht o env

--- src/domain/utils/TKIconUrl.ts
+++ src/domain/utils/TKIconUrl.ts
@@ -7,16 +7,8 @@ export function TKIconUrl(
   mode: IconPosition = IconPosition.INTERFACE
 ): string {
   if (mode === IconPosition.INTERFACE) {
-    return (
-      "https://raw.githubusercontent.com/CartONG/opsmap-icons/main/" +
-      name +
-      ".svg"
-    );
+    return process.env.VUE_APP_ICONS_BASE_URL + name + ".svg";
   } else {
-    return (
-      "https://raw.githubusercontent.com/CartONG/opsmap-icons/main/" +
-      name +
-      ".png"
-    );
+    return process.env.VUE_APP_ICONS_BASE_URL + name + ".png";
   }
 }
